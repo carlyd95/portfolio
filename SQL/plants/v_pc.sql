543fbe8269b8e2739072ed50de394f5ea201efd4
carlyd95 <dcarltondunlap@hotmail.com>
2022-05-24 21:42:25 -0400

    fixed v_pc.sql

--- SQL/plants/v_pc.sql
+++ SQL/plants/v_pc.sql
@@ -1,16 +1,18 @@
-create VIEW `v_field_number_dupes` as
-	select `v_pc`.`ck` AS `ck`,
-	`sub`.`plant_field_number` AS `plant_field_number`,
-	`sub`.`count` AS `count`
-	from (((select `v_pc`.`plant_field_number` AS `plant_field_number`, count(`v_pc`.`ck`) AS `count`
-	from `pc`.`v_pc`
-	where `v_pc`.`plant_field_number` regexp '[0-9]'
-	group by `v_pc`.`plant_field_number`
-	having count(`v_pc`.`plant_field_number`) > 2)) `sub`
-	join `pc`.`v_pc` on(`sub`.`plant_field_number` = `v_pc`.`plant_field_number`))
-	order by count desc, plant_field_number asc;
-
-
-
-
-		
+create or replace view v_pc as
+	select concat(lpad(hex(`pc`.`plant`.`id_genus`),2,'0'),lpad(hex(`pc`.`plant`.`id_species`),2,'0'),concat('-',lpad(hex(`pc`.`plant`.`id_plant`),3,'0'))) AS `ck`,
+		`pc`.`genus`.`genus` AS `genus`,
+		`pc`.`species`.`species` AS `species`,
+		`pc`.`plant`.`plant_infraspecies` AS `plant_infraspecies`,
+		`pc`.`plant`.`plant_locality` AS `plant_locality`,
+		`pc`.`plant`.`plant_field_number` AS `plant_field_number`,
+		`pc`.`source`.`source` AS `plant_source`,
+		`pc`.`plant`.`plant_cultivar` AS `plant_cultivar`,
+		`pc`.`plant`.`plant_parentage` AS `plant_parentage`,
+		`pc`.`plant`.`plant_note` AS `plant_note`,
+		`pc`.`plant`.`plant_empty` AS `plant_empty`,
+		`pc`.`plant`.`plant_has_pic` AS `plant_has_pic`,
+		`pc`.`plant`.`plant_source` AS `id_plant_source` 
+	from (((`pc`.`plant` 
+	join `pc`.`species` on(`pc`.`plant`.`id_genus` = `pc`.`species`.`id_genus` and `pc`.`plant`.`id_species` = `pc`.`species`.`id_species`))
+	join `pc`.`genus` on(`pc`.`plant`.`id_genus` = `pc`.`genus`.`id_genus`))
+	join `pc`.`source` on(`pc`.`plant`.`plant_source` = `pc`.`source`.`id_source`));
